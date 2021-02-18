818a4432a3387c538b64dd5f78a4ced1133e565e
prateekbharadwaj <prateekbharadwaj2533@gmail.com>
2021-02-18 22:22:05 +0530

    Changes done for deployment

--- config.php
+++ config.php
@@ -1,10 +1,17 @@
+
 <?php
-	$conn = mysqli_connect("localhost", "root", "", "banking");
-	
-	if(!$conn){
-		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
-	}
-
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-03.cleardb.com"];
+$cleardb_username = $cleardb_url["b080c5734ce3f6"];
+$cleardb_password = $cleardb_url["94d9f040"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+if(!$conn){
+	die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+}
 ?>
-
 
